7102b28900e72a6424de2d24c4e90d4833d1cdc4
thisisfocus-k <75963158+thisisfocus-k@users.noreply.github.com>
2020-12-28 11:14:35 +0700

    Update x2.c

--- x2.c
+++ x2.c
@@ -2,10 +2,11 @@
 int main(void){
     int i,j; 
     for(i = 2; i < 13; i++){
+    	printf("(%d)\n",i);
     	for(j = 1; j < 13; j++){
     		printf("%d x %d = %d\n",i,j,i*j);
 		}
+		printf("\n");
 	}
-        
     return 0;
 }
